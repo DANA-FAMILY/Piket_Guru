fcba4948cbd98e045a54170c05344ce08291940b
EgaSuzana <142204907+EgaSuzana@users.noreply.github.com>
2024-11-22 23:28:31 +0700

    Update index.vue

--- pages/created/index.vue
+++ pages/created/index.vue
@@ -1,33 +1,42 @@
 <template>
   <div class="container-fluid">
+    <!-- Tombol Kembali -->
     <div class="row">
-      <div class="col-lg-12">
-        <h2 class="text-center my-3">Profil Keluarga Dana</h2>
+      <div class="col-lg-12 d-flex justify-content-end p-3">
+        <NuxtLink to="/">
+          <button class="btn btn-primary">Kembali</button>
+        </NuxtLink>
       </div>
     </div>
+
+    <!-- Foto Utama -->
+    <div class="row">
+      <div class="col-lg-12 d-flex justify-content-center align-items-center my-3">
+        <img src="/assets/img/poto1.jpg" alt="Foto Keluarga" width="500" height="300" class="img-fluid rounded">
+      </div>
+    </div>
+
+    <!-- Profil Anggota -->
     <div class="row">
       <!-- Anggota 1 -->
       <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
-        <img src="/assets/img/poto1.jpg" alt="Anggota 1" width="200" height="200" class="rounded-circle">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 1" width="150" height="150" class="rounded-circle">
         <h3 class="mt-3">Nama Anggota 1</h3>
-        <p>Deskripsi singkat tentang anggota 1, misalnya hobi, pekerjaan, atau kepribadian.</p>
+        <p>Deskripsi singkat tentang anggota 1.</p>
       </div>
       <!-- Anggota 2 -->
       <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
-        <img src="/assets/img/poto1.jpg" alt="Anggota 2" width="200" height="200" class="rounded-circle">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 2" width="150" height="150" class="rounded-circle">
         <h3 class="mt-3">Nama Anggota 2</h3>
-        <p>Deskripsi singkat tentang anggota 2, misalnya hobi, pekerjaan, atau kepribadian.</p>
+        <p>Deskripsi singkat tentang anggota 2.</p>
       </div>
       <!-- Anggota 3 -->
       <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
-        <img src="/assets/img/poto1.jpg" alt="Anggota 3" width="200" height="200" class="rounded-circle">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 3" width="150" height="150" class="rounded-circle">
         <h3 class="mt-3">Nama Anggota 3</h3>
-        <p>Deskripsi singkat tentang anggota 3, misalnya hobi, pekerjaan, atau kepribadian.</p>
+        <p>Deskripsi singkat tentang anggota 3.</p>
       </div>
     </div>
-    <NuxtLink to="/" class="p-3 m-2 d-flex justify-content-center">
-      <button class="btn btn-primary">Kembali</button>
-    </NuxtLink>
   </div>
 </template>
 
@@ -35,13 +44,16 @@
   .d-flex {
     display: flex;
   }
-  .navbar-brand {
-    display: flex;
-  }
-  h2 {
-    margin-bottom: 30px;
-  }
   .rounded-circle {
     border: 2px solid #ccc;
+  }
+  .btn-primary {
+    margin-left: 20px;
+  }
+  h3 {
+    margin-top: 15px;
+  }
+  p {
+    text-align: center;
   }
 </style>
