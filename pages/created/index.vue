2048cbeb3a24540a2e0845e95efe8198238a847a
EgaSuzana <142204907+EgaSuzana@users.noreply.github.com>
2024-11-22 23:37:38 +0700

    Update index.vue

--- pages/created/index.vue
+++ pages/created/index.vue
@@ -17,24 +17,29 @@
     </div>
 
     <!-- Profil Anggota -->
-    <div class="row">
+    <div class="row d-flex justify-content-center">
       <!-- Anggota 1 -->
-      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+      <div class="col-lg-3 col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center my-3">
         <img src="/assets/img/poto1.jpg" alt="Anggota 1" width="150" height="150" class="rounded-circle">
-        <h3 class="mt-3">Nama Anggota 1</h3>
-        <p>Deskripsi singkat tentang anggota 1.</p>
+        <h3 class="mt-3">Ega Suzana</h3>
+        <p class="text-center">Front end</p>
       </div>
       <!-- Anggota 2 -->
-      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+      <div class="col-lg-3 col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center my-3">
         <img src="/assets/img/poto1.jpg" alt="Anggota 2" width="150" height="150" class="rounded-circle">
-        <h3 class="mt-3">Nama Anggota 2</h3>
-        <p>Deskripsi singkat tentang anggota 2.</p>
+        <h3 class="mt-3">Irgi Cahya</h3>
+        <p class="text-center">Desain</p>
       </div>
       <!-- Anggota 3 -->
-      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+      <div class="col-lg-3 col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center my-3">
         <img src="/assets/img/poto1.jpg" alt="Anggota 3" width="150" height="150" class="rounded-circle">
-        <h3 class="mt-3">Nama Anggota 3</h3>
-        <p>Deskripsi singkat tentang anggota 3.</p>
+        <h3 class="mt-3">Sani Sahrul M</h3>
+        <p class="text-center">Backend</p>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 d-flex flex-column justify-content-center align-items-center my-3">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 3" width="150" height="150" class="rounded-circle">
+        <h3 class="mt-3">Gilang A</h3>
+        <p class="text-center">Prototipe</p>
       </div>
     </div>
   </div>
