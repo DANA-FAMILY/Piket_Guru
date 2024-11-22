c426e3ccb65e165293dd3b6909afe041511abd08
EgaSuzana <142204907+EgaSuzana@users.noreply.github.com>
2024-11-22 23:22:47 +0700

    Update index.vue

--- pages/created/index.vue
+++ pages/created/index.vue
@@ -2,16 +2,31 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-12">
-        <h2 class="text-center my-3">Created By: Dana Family</h2>
-      </div>
-      <div class="col-lg-12 d-flex justify-content-center align-items-center">
-        <a class="navbar-brand" href="#">
-          <img src="/assets/img/poto1.jpg" alt="" width="500" height="300">
-        </a>
+        <h2 class="text-center my-3">Profil Keluarga Dana</h2>
       </div>
     </div>
-    <NuxtLink to="/" class="p-3 m-2">
-          <Button>Kembali</Button>
+    <div class="row">
+      <!-- Anggota 1 -->
+      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 1" width="200" height="200" class="rounded-circle">
+        <h3 class="mt-3">Nama Anggota 1</h3>
+        <p>Deskripsi singkat tentang anggota 1, misalnya hobi, pekerjaan, atau kepribadian.</p>
+      </div>
+      <!-- Anggota 2 -->
+      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 2" width="200" height="200" class="rounded-circle">
+        <h3 class="mt-3">Nama Anggota 2</h3>
+        <p>Deskripsi singkat tentang anggota 2, misalnya hobi, pekerjaan, atau kepribadian.</p>
+      </div>
+      <!-- Anggota 3 -->
+      <div class="col-lg-4 d-flex flex-column justify-content-center align-items-center my-3">
+        <img src="/assets/img/poto1.jpg" alt="Anggota 3" width="200" height="200" class="rounded-circle">
+        <h3 class="mt-3">Nama Anggota 3</h3>
+        <p>Deskripsi singkat tentang anggota 3, misalnya hobi, pekerjaan, atau kepribadian.</p>
+      </div>
+    </div>
+    <NuxtLink to="/" class="p-3 m-2 d-flex justify-content-center">
+      <button class="btn btn-primary">Kembali</button>
     </NuxtLink>
   </div>
 </template>
@@ -23,9 +38,10 @@
   .navbar-brand {
     display: flex;
   }
-  h2.ms-3 {
-    margin-left: 20px;
+  h2 {
+    margin-bottom: 30px;
+  }
+  .rounded-circle {
+    border: 2px solid #ccc;
   }
 </style>
-
-
